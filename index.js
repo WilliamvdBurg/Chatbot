15092099d623ac0a3aaf334cf80716d9c1029140
William van der Burg <William@F12s-Mac-mini.fritz.box>
2016-08-30 15:28:28 +0200

    reactie tekst veranderd

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, "Wat lief dat je dat vraagt, " + text.substring(0, 200))
         }
     }
     res.sendStatus(200)
