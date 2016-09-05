62af5a0c3bfb09ded7d2a57b06c575994be6bfb7
William <s1079684@student.hsleiden.nl>
2016-09-05 12:56:59 +0200

    commented other shii out

--- index.js
+++ index.js
@@ -37,19 +37,19 @@ app.listen(app.get('port'), function() {
 
 app.post('/webhook/', function (req, res) {
     console.log('post request on /webhook/');
-    console.log('message body is', req.body);
+    console.log('message body is', req.body[0].messaging);
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
-        if (event.message === 'hello') {
+        if (event.message.text === 'hello') {
             // Let's reply back hello
             message = 'Hello yourself! I am a chat bot. You can say "show me pics of corgis"'
             reply(sender, message)
         }
         if (event.message && event.message.text) {
             text = event.message.text
-            sendTextMessage(sender, "" + text.substring(0, 200))
+            // sendTextMessage(sender, "" + text.substring(0, 200))
         }
 
 
@@ -79,134 +79,3 @@ app.post('/webhook/', function (req, res) {
 // })
 
 var token = "EAAH6aBRRwRIBAAztsST3yW36UMjwAXW18gx5jfDDHGL0fgzI9zja5TPBtUiVXIVS9zaZASfaSXOJCqb0ZBXzWQF1LUWiZBbcRXqcPTz1atCTvQFF4cvodOJ7dmlTJQMFIAsL1uxiJtFjasn4ls4Ex2WeZA3rPrRKmXhMcQf9IQZDZD"
-
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-// app.post('/webhook/', function (req, res) {
-//     messaging_events = req.body.entry[0].messaging
-//     for (i = 0; i < messaging_events.length; i++) {
-//         event = req.body.entry[0].messaging[i]
-//         sender = event.sender.id
-//         if (event.message && event.message.text) {
-//             text = event.message.text
-//             if (text === 'Generic') {
-//                 sendGenericMessage(sender)
-//                 continue
-//             }
-//             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-//         }
-//     }
-//     res.sendStatus(200)
-// })
-//
-// app.post('/webhook/', function (req, res) {
-//     messaging_events = req.body.entry[0].messaging
-//     for (i = 0; i < messaging_events.length; i++) {
-//         event = req.body.entry[0].messaging[i]
-//         sender = event.sender.id
-//         if (event.message && event.message.text) {
-//             text = event.message.text
-//             if (text === 'Generic') {
-//                 sendGenericMessage(sender)
-//                 continue
-//             }
-//             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-//         }
-//         if (event.postback) {
-//             text = JSON.stringify(event.postback)
-//             sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-//             continue
-//         }
-//     }
-//     res.sendStatus(200)
-// })
-
-
-//
-// app.post('/webhook/', function (req, res) {
-//     messaging_events = req.body.entry[0].messaging
-//     for (i = 0; i < messaging_events.length; i++) {
-//         event = req.body.entry[0].messaging[i]
-//         sender = event.sender.id
-//         if (event.message && event.message.text) {
-//             text = event.message.text
-//             sendTextMessage(sender, "" + text.substring(0, 200))
-//         }
-//         if (event.message === 'hello') {
-//             // Let's reply back hello
-//             message = 'Hello yourself! I am a chat bot. You can say "show me pics of corgis"'
-//             reply(sender, message)
-//         } else {
-//
-//         }
-//     }
-//     res.sendStatus(200)
-// })
