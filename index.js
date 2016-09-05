d601565144f06883b21a1a08019465c05d41dcb4
William <s1079684@student.hsleiden.nl>
2016-09-05 12:50:36 +0200

    test log

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.listen(app.get('port'), function() {
 
 
 app.post('/webhook/', function (req, res) {
+    console.log('post request on /webhook/');
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
