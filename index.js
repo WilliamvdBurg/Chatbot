e260e3a1bf77ce30bc7ab278e4c8fafc763285a0
William <s1079684@student.hsleiden.nl>
2016-09-06 10:22:01 +0200

    helloback21

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/webhook/', function (req, res) {
                 sendTextMessage(sender, ' Hello! Im BotVrendly, how can i help you?');
                 continue
             }
-            else if (text == 'doei' || 'Doei' || 'dag' || 'Dag' || 'Bye' || 'bye' || 'doeg' || 'Doeg' || 'Totziens' || 'totziens') {
+            else if (text === 'doei' || 'Doei' || 'dag' || 'Dag' || 'Bye' || 'bye' || 'doeg' || 'Doeg' || 'Totziens' || 'totziens') {
                 sendTextMessage(sender, "Thank you, have a nice day!!");
                 continue
             }
