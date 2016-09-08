eadebecbfa213c0f7920d5a671fe296a3f0ba78a
William <s1079684@student.hsleiden.nl>
2016-09-08 13:03:23 +0200

    new2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const WIT_TOKEN = process.env.WIT_TOKEN || '3TTVP67B6TLHAXVM2E5LC5T73FJQJGIC;'
+const WIT_TOKEN = process.env.WIT_TOKEN || '3TTVP67B6TLHAXVM2E5LC5T73FJQJGIC';
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
@@ -11,7 +11,7 @@ if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'witai_bot_will';
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'vrendly_bot_will';
 
 module.exports = {
   WIT_TOKEN: WIT_TOKEN,
