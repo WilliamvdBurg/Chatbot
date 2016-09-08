be7d6a2ad9b156adf933ab792a4f5fee232a8b12
William <s1079684@student.hsleiden.nl>
2016-09-08 12:48:59 +0200

    new

---
+++ config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const WIT_TOKEN = process.env.WIT_TOKEN || '3TTVP67B6TLHAXVM2E5LC5T73FJQJGIC;'
+if (!WIT_TOKEN) {
+  throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
+}
+
+
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAH6aBRRwRIBAAztsST3yW36UMjwAXW18gx5jfDDHGL0fgzI9zja5TPBtUiVXIVS9zaZASfaSXOJCqb0ZBXzWQF1LUWiZBbcRXqcPTz1atCTvQFF4cvodOJ7dmlTJQMFIAsL1uxiJtFjasn4ls4Ex2WeZA3rPrRKmXhMcQf9IQZDZD';
+if (!FB_PAGE_TOKEN) {
+	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
+}
+
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'witai_bot_will';
+
+module.exports = {
+  WIT_TOKEN: WIT_TOKEN,
+  FB_PAGE_TOKEN: FB_PAGE_TOKEN,
+  FB_VERIFY_TOKEN: FB_VERIFY_TOKEN,
+}
