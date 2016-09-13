fdbbfe3f34ac23e12134a45d037f444bb7fa7943
William <s1079684@student.hsleiden.nl>
2016-09-13 11:27:03 +0200

    say hi

--- bot.js
+++ bot.js
@@ -33,10 +33,6 @@ var findOrCreateSession = function (fbid) {
 
 var read = function (sender, message, reply)
 { 	console.log('read')
-	if(chatIsClean || todayNothingSend){
-	send('hallo')
-	}
-
 	if (message === 'hello') {
 		// Let's reply back hello
 		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
