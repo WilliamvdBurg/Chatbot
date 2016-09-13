c64d70ce3c7785ba4f6c5db24b4d26eff0c45af2
William <s1079684@student.hsleiden.nl>
2016-09-13 11:32:24 +0200

    say hi

--- bot.js
+++ bot.js
@@ -37,18 +37,18 @@ var read = function (sender, message, reply)
 		// Let's reply back hello
 		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
 		reply(sender, message)
-}
-	else if (message === 'yo') {
+	}
+	if (message === 'yo') {
 		// Let's reply back hello
 		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
 		reply(sender, message)
 	}
-	else if (message === 'heey') {
+	if (message === 'heey') {
 		// Let's reply back hello
 		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
 		reply(sender, message)
 
-	 else {
+	} else {
 		// Let's find the user
 		var sessionId = findOrCreateSession(sender)
 		// Let's forward the message to the Wit.ai bot engine
