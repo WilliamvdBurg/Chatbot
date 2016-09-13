3467085e21b07a5cdaf2c4b0e615e2c2afd14c16
William <s1079684@student.hsleiden.nl>
2016-09-13 10:40:23 +0200

    say hi

--- bot.js
+++ bot.js
@@ -37,6 +37,17 @@ var read = function (sender, message, reply)
 		// Let's reply back hello
 		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
 		reply(sender, message)
+	}
+	else if (message === 'yo') {
+		// Let's reply back hello
+		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
+		reply(sender, message)
+	}
+	else if (message === 'heey') {
+		// Let's reply back hello
+		message = 'Hello! I am VrendlyBot. How can i help you today? Typ Help for more info.'
+		reply(sender, message)
+
 	} else {
 		// Let's find the user
 		var sessionId = findOrCreateSession(sender)
