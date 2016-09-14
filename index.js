e42e6fd6448fef72e045871f6faaeba182ff3ef6
William <s1079684@student.hsleiden.nl>
2016-09-14 12:36:44 +0200

    say hi

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ var request = require('request');
 
 //Lets try to make a HTTPS GET request to modulus.io's website.
 //All we did here to make HTTPS call is changed the `http` to `https` in URL.
-request('https://modulus.io', function (error, response, body) {
-    //Check for error
-    if(error){
-        return console.log('Error:', error);
-    }
-
-    //Check f
-    // or right status code
-    if(response.statusCode !== 200){
-        return console.log('Inval' +
-            'id Status Code Returned:', response.statusCode);
-    }
-
-    //All is good. Print the body
-    console.log(body); // Show the HTML for the Modulus homepage.
-
-});
+// request('https://modulus.io', function (error, response, body) {
+//     //Check for error
+//     if(error){
+//         return console.log('Error:', error);
+//     }
+//
+//     //Check f
+//     // or right status code
+//     if(response.statusCode !== 200){
+//         return console.log('Inval' +
+//             'id Status Code Returned:', response.statusCode);
+//     }
+//
+//     //All is good. Print the body
+//     console.log(body); // Show the HTML for the Modulus homepage.
+//
+// });
 // LETS MAKE A SERVER!
 var app = express()
 app.set('port', (process.env.PORT) || 5000)
