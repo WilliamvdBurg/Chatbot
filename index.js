3f5b03fa8943f27566e6776248995357c07ec51d
William <s1079684@student.hsleiden.nl>
2016-09-14 11:54:11 +0200

    say hi

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ var Config = require('./config')
 var FB = require('./connectors/facebook')
 var Bot = require('./bot')
 
-//Lets try to make a HTTP GET request to modulus.io's website.
-request.get('http://www.modulus.io', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body); // Show the HTML for the Modulus homepage.
+var request = require('request');
+
+//Lets try to make a HTTPS GET request to modulus.io's website.
+//All we did here to make HTTPS call is changed the `http` to `https` in URL.
+request('https://modulus.io', function (error, response, body) {
+    //Check for error
+    if(error){
+        return console.log('Error:', error);
     }
+
+    //Check for right status code
+    if(response.statusCode !== 200){
+        return console.log('Invalid Status Code Returned:', response.statusCode);
+    }
+
+    //All is good. Print the body
+    console.log(body); // Show the HTML for the Modulus homepage.
+
 });
 // LETS MAKE A SERVER!
 var app = express()
