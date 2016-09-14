829f8b7f43380eafbe54f6d5b0cc49f45b2ac44b
William <s1079684@student.hsleiden.nl>
2016-09-14 16:25:38 +0200

    say hi

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ var read = function (sender, message, reply)
 			}
 		})
 	}
-	if (message.response == 0) {
+	if (message.response == null) {
 		message = 'I AM NOT SURE IF I  UNDERSTAND WHAT YOU  ARE TALKING ABOUT.'
 		reply(sender, message)
 	}
