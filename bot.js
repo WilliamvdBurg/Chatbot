b013ab5309045c51dbcb1b59b16d01eef7a0ca1d
William <s1079684@student.hsleiden.nl>
2016-09-14 16:12:19 +0200

    say hi

--- bot.js
+++ bot.js
@@ -128,10 +128,10 @@ var read = function (sender, message, reply)
 		message = 'No problem, i am here for you!'
 		reply(sender, message)
 	}
-	// else if (response == 0) {
-	// 	message = 'I AM NOT SURE IF I  UNDERSTAND WHAT YOU  ARE TALKING ABOUT.'
-	// 	reply(sender, message)
-	// }
+	else if (message.response == 0) {
+		message = 'I AM NOT SURE IF I  UNDERSTAND WHAT YOU  ARE TALKING ABOUT.'
+		reply(sender, message)
+	}
 	else {
 		// Let's find the user
 		var sessionId = findOrCreateSession(sender)
