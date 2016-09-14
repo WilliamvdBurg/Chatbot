ff64ed938e85ada16de7bc78a20544e729e218ee
William <s1079684@student.hsleiden.nl>
2016-09-14 14:28:04 +0200

    say hi

--- services/wit.js
+++ services/wit.js
@@ -18,6 +18,32 @@ var firstEntityValue = function (entities, entity) {
 	return typeof val === 'object' ? val.value : val
 }
 
+// Require the module
+var Forecast = require('forecast');
+
+// Initialize
+var forecast = new Forecast({
+	service: 'forecast.io',
+	key: 'your-api-key',
+	units: 'celcius', // Only the first letter is parsed
+	cache: true,      // Cache API requests?
+	ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+		minutes: 27,
+		seconds: 45
+	}
+});
+
+// Retrieve weather information from coordinates (Sydney, Australia)
+forecast.get([-33.8683, 151.2086], function(err, weather) {
+	if(err) return console.dir(err);
+	console.dir(weather);
+});
+
+// Retrieve weather information, ignoring the cache
+forecast.get([-33.8683, 151.2086], true, function(err, weather) {
+	if(err) return console.dir(err);
+	console.dir(weather);
+});
 
 var actions = {
 	say (sessionId, context, message, cb) {
