43efa3e3f6ab7de0a107a855813f39fb79c63c3c
William <s1079684@student.hsleiden.nl>
2016-09-16 10:33:36 +0200

    hdudd

--- index.js
+++ index.js
@@ -198,83 +198,30 @@ function sendTextMessage(sender, text) {
 
 
 
-function sendWebsiteMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text":"Hello, do you wanna start the test?",
-                "buttons":[
-                            {
-                                "type":"postback",
-                                "title":"ja",
-                                "payload" : "StartTest"
-                            },
-                            {
-                              "type":"postback",
-                              "title":"Nee",
-                              "payload" : "StartTest"
-
-                            },
-
-                          ]
-                        }
-                      }
-                    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: token},
-        method: 'POST',
-        json: {
-            recipient: {id: sender},
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-
-}
-
-
-
-
-// function sendGenericMessage(sender) {
+// function sendWebsiteMessage(sender) {
 //     messageData = {
 //         "attachment": {
 //             "type": "template",
 //             "payload": {
-//                 "template_type": "generic",
-//                 "elements": [{
-//                     "title": "First card",
-//                     "subtitle": "Element #1 of an hscroll",
-//                     "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-//                     "buttons": [{
-//                         "type": "web_url",
-//                         "url": "https://www.oculus.com/",
-//                         "title": "web url"
-//                     }, {
-//                         "type": "postback",
-//                         "title": "Postback",
-//                         "payload": "Payload for first element in a generic bubble",
-//                     }],
-//                 }, {
-//                     "title": "Second card",
-//                     "subtitle": "Element #2 of an hscroll",
-//                     "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-//                     "buttons": [{
-//                         "type": "postback",
-//                         "title": "Postback",
-//                         "payload": "Payload for second element in a generic bubble",
-//                     }],
-//                 }]
-//             }
-//         }
-//     }
+//                 "template_type": "button",
+//                 "text":"Hello, do you wanna start the test?",
+//                 "buttons":[
+//                             {
+//                                 "type":"postback",
+//                                 "title":"ja",
+//                                 "payload" : "StartTest"
+//                             },
+//                             {
+//                               "type":"postback",
+//                               "title":"Nee",
+//                               "payload" : "StartTest"
+//
+//                             },
+//
+//                           ]
+//                         }
+//                       }
+//                     }
 //     request({
 //         url: 'https://graph.facebook.com/v2.6/me/messages',
 //         qs: {access_token: token},
@@ -292,6 +239,59 @@ function sendWebsiteMessage(sender) {
 //     })
 //
 // }
+
+
+
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.oculus.com/",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: token},
+        method: 'POST',
+        json: {
+            recipient: {id: sender},
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+
+}
 
 
 
