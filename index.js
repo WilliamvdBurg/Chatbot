01ae7714ed6aaed97a514c86e95b03144d3bacb6
William <s1079684@student.hsleiden.nl>
2016-09-19 10:09:03 +0200

    hello

--- index.js
+++ index.js
@@ -48,6 +48,9 @@ app.post('/webhook/', function (req, res) {
                 sendWebsiteMessage(sender)
 
             }
+            if (text == 'testresultaten'){
+                sendTextMessage(sender, 'Vraag 1: antwoord' + response,vraag = 0)
+            }
             if (text == 'Ja'){
                 sendWebsiteMessage(sender, "Oke! dankuwel voor het invullen van de vragenlijst. Totziens!!")
             }
