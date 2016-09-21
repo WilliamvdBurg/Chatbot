1f1f96f315c829610ee60f7bd6a91ceef54fca68
William <s1079684@student.hsleiden.nl>
2016-09-21 16:13:19 +0200

    hhahahs

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.post('/webhook/', function (req, res) {
                     } else if (response.body.error) {
                         console.log('Error: ', response.body.error)
                     }else{
-                        askQuestion(sender)
+                        sendGenericMessage(sender)
                     }
                 })
             }
@@ -387,19 +387,7 @@ function sendOnderwijsMessage(sender) {
     })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-function getEvaluation56(sender) {
+function getEvaluation56(sender){
     request({
         url: 'https://staging-api-portal.evalytics.nl/evaluation/getDetails/56',
         qs: {access_token: token},
@@ -407,81 +395,21 @@ function getEvaluation56(sender) {
         headers: {
             ['access-token']: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtaXNzaW9ucyI6eyJldmFsdWF0aW9uIjp7ImdldERldGFpbHMiOiJ0cnVlIiwicG9zdEFuc3dlcnMiOiJ0cnVlIn19LCJjb2RlIjoiam9yZGlpc2dlayIsImV2YWx1YXRpb25JZCI6IjU2Iiwic3ViIjoiMTQiLCJpYXQiOjE0NzQzNzQ0NjAsImV4cCI6MTQ3NDQ2MDg2MCwiYXVkIjpbInd3dy5ldmFseXRpY3MubmwiXSwiaXNzIjoiRXZhbHl0aWNzIn0.KIQs3T0w24PVtvMWcmUQmw7UJdTk-EzqMSj1h1HS4pY'
         }
-    }).then(function (result) {
+    }).then(function(result){
         // console.log(result)
         var data = JSON.parse(result);
         var evaluation = data.results[0];
         console.log(evaluation)
 
         var openQuestions = [];
-        _.forEach(evaluation.blocks[0].questionSets, function (questionset) {
+        _.forEach(evaluation.blocks[0].questionSets, function(questionset){
             console.log(questionset)
         })
 
-    }).catch(function (error) {
+    }).catch(function(error){
         console.log(error);
     })
-
-        // .then(function askQuestion(questionId, sender) {
-        //     var question = _.find(questions, ['id', questionId]);
-        //     var quickReplies = [];
-
-            // if (question.scale.input === 'rating') {
-            //     // 1 t/m 10 afhandeling
-            // } else if (question.scale.input === 'boolean') {
-            //     // Ja/nee afhandeling
-            // } else if (question.scale.input === 'text') {
-            //     // Open vraag
-            // } else {
-            //     question.scale.scalenNl.forEach(function (scale) {
-            //         quickReplies.push({
-            //             title: scale.value
-            //             // payload
-            //             // content type
-            //         })
-            //
-            //     }).catch(function (error) {
-            //         console.log(error);
-            //     })
-        // (function sendGenericMessage(sender) {
-        //                 messageData = [{
-        //                     text: question.questionNl,
-        //                     quick_replies: quickReplies
-        //
-        //                 }];
-        //             })
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function  sendInformaticaMessage(sender) {
@@ -805,8 +733,32 @@ request({
 //     })
 //
 // }
+function askQuestion(questionId, sender) {
+    var question = _.find(questions, ['id', questionId]);
 
+    var quickReplies = [];
 
+    if(question.scale.input === 'rating'){
+        // 1 t/m 10 afhandeling
+    } else if(question.scale.input === 'boolean'){
+        // Ja/nee afhandeling
+    } else if(question.scale.input === 'text'){
+        // Open vraag
+    } else {
+        question.scale.scalenNl.forEach(function(scale) {
+            quickReplies.push({
+                title: scale.value
+                // payload
+                // content type
+            })
+        });
+    }
+
+    var messageData = [{
+        text: question.questionNl,
+        quick_replies: quickReplies
+    }];
+}
 
 // quickreplie buttons aanmaak. Zijn er 10 want er cijfers gaan van 1 t/m 10
 
