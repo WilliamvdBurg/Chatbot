43ef101a723ff6b772aa20d8fa3a168e1b5a40f4
William <s1079684@student.hsleiden.nl>
2016-09-22 15:33:01 +0200

    hhahahs

--- index.js
+++ index.js
@@ -350,14 +350,14 @@ var i = 1;
     }
     else {
         console.log('jsdbaksjdb');
-        question.scale.scalenNl.forEach(scale); {
+        question.scale.scalenNl.forEach(function(scale) {
             quickReplies.push({
                 content_type: 'text',
                 title: scale.value,
                 payload: 'DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED'
             })
-
-        };
+            console.log('jajajajaaajajajajaj');
+        });
     }
 
     // quick_replies: [{
