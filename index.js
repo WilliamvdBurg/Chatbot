fa39967c52bdc8e9fc677b1b4d2738b2fefd0755
William <s1079684@student.hsleiden.nl>
2016-09-28 10:20:44 +0200

    hhahahs

--- index.js
+++ index.js
@@ -334,27 +334,7 @@ function startVragen(userInput)
             .catch(function (error) {
                 console.log(error);
             })
-    if (sessies[recipient].vragensessie && questionSet) {
 
-        if (text > 10) {
-            sendTextMessage(sender, 'error, antwoord onbekend!')
-        }
-        if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
-            sessies[recipient].vraag++;
-            sessies[recipient].answers.push(text);
-            console.log(' answers zijn',sessies[recipient].answers);
-            // moet gereset worden + verzonden.
-        }
-
-        if (questionSet[sessies[recipient].vraag]) {
-            askQuestion(questionSet[sessies[recipient].vraag], sender);
-        }
-
-        if (sessies[recipient].vraag >= questionSet.length) {
-            sendKlaarMessage(sender, 'alle vragen zijn beantwoord, bent u zeker over uw antwoorden?')
-        }
-
-    }
     }
 
 
@@ -421,7 +401,27 @@ function getEvaluationData(id, accessToken) {
             questionSet = questionset.questions;
             console.log(questionSet);
         });
+        if (sessies[recipient].vragensessie && questionSet) {
 
+            if (text > 10) {
+                sendTextMessage(sender, 'error, antwoord onbekend!')
+            }
+            if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
+                sessies[recipient].vraag++;
+                sessies[recipient].answers.push(text);
+                console.log(' answers zijn',sessies[recipient].answers);
+                // moet gereset worden + verzonden.
+            }
+
+            if (questionSet[sessies[recipient].vraag]) {
+                askQuestion(questionSet[sessies[recipient].vraag], sender);
+            }
+
+            if (sessies[recipient].vraag >= questionSet.length) {
+                sendKlaarMessage(sender, 'alle vragen zijn beantwoord, bent u zeker over uw antwoorden?')
+            }
+
+        }
         return questionSet;
     }).catch(function (error) {
         console.log(error);
