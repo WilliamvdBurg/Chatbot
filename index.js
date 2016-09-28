e3bf47763632dbf99e473ba1ee042135dada94d7
William <s1079684@student.hsleiden.nl>
2016-09-28 10:27:57 +0200

    hhahahs

--- index.js
+++ index.js
@@ -133,8 +133,7 @@ app.post('/webhook/', function (req, res) {
                 console.log("code is getypt", text)
                 startVragen(text);
             }
-            console.log('sessies[recipient].vragensessie: ', sessies[recipient].vragensessie);
-            console.log('questionSet: ', questionSet);
+
             // if (sessies[recipient].vragensessie && questionSet) {
             //
             //     if (text > 10) {
@@ -328,11 +327,10 @@ function startVragen(userInput)
                 return getEvaluationData(evaluationId, accessToken);
             })
             .then(function (questionSet) {
-                console.log('askquetsions word aangeroepen')
                 askQuestion(questionSet[sessies[recipient].vraag], sender);
             })
             .catch(function (error) {
-                console.log(error);
+                console.log('startvragen', error);
             })
 
     }
@@ -378,7 +376,7 @@ function authenticateCode(code) {
         var data = JSON.parse(result);
         return data.accessToken;
     }).catch(function (error) {
-        console.log(error);
+        console.log('authenticateCode', error);
     })
 
 
@@ -401,30 +399,10 @@ function getEvaluationData(id, accessToken) {
             questionSet = questionset.questions;
             console.log(questionSet);
         });
-        if (sessies[recipient].vragensessie && questionSet) {
 
-            if (text > 10) {
-                sendTextMessage(sender, 'error, antwoord onbekend!')
-            }
-            if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
-                sessies[recipient].vraag++;
-                sessies[recipient].answers.push(text);
-                console.log(' answers zijn',sessies[recipient].answers);
-                // moet gereset worden + verzonden.
-            }
-
-            if (questionSet[sessies[recipient].vraag]) {
-                askQuestion(questionSet[sessies[recipient].vraag], sender);
-            }
-
-            if (sessies[recipient].vraag >= questionSet.length) {
-                sendKlaarMessage(sender, 'alle vragen zijn beantwoord, bent u zeker over uw antwoorden?')
-            }
-
-        }
         return questionSet;
     }).catch(function (error) {
-        console.log(error);
+        console.log('getEvaluationData', error);
     });
 }
 
