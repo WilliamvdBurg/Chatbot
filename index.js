d41de267dbfebc91e7ff3455dc1c52147845b1d4
William <s1079684@student.hsleiden.nl>
2016-09-28 17:55:24 +0200

    hhahahs

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ app.post('/webhook/', function (req, res) {
 
 // hierin word de assay aangevraagd zodat deze in het rest van de code gebruikt kan worden.  de token is een token die je terugkrijgt nadat je je eerste token meegeeft op de site van evalytics. deze code geeft je de vragen terug.
     function getEvaluationData(id, accessToken) {
-        console.log('we gaan nu een call maken om de details te vragen', id, accessToken);
+        console.log('we gaan nu een call maken om de details te vragen', id, accessToken.accessToken);
         return request({
             url: 'https://staging-api-portal.evalytics.nl/evaluation/getDetails/' + id,
             method: 'GET',
