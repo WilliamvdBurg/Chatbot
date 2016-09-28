66925bb4dd070fbb6c021363fff6064b1bbcab80
William <s1079684@student.hsleiden.nl>
2016-09-28 10:17:58 +0200

    hhahahs

--- index.js
+++ index.js
@@ -135,27 +135,27 @@ app.post('/webhook/', function (req, res) {
             }
             console.log('sessies[recipient].vragensessie: ', sessies[recipient].vragensessie);
             console.log('questionSet: ', questionSet);
-            if (sessies[recipient].vragensessie && questionSet) {
-
-                if (text > 10) {
-                    sendTextMessage(sender, 'error, antwoord onbekend!')
-                }
-                if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
-                    sessies[recipient].vraag++;
-                    sessies[recipient].answers.push(text);
-                    console.log(' answers zijn',sessies[recipient].answers);
-                    // moet gereset worden + verzonden.
-                }
-
-                if (questionSet[sessies[recipient].vraag]) {
-                    askQuestion(questionSet[sessies[recipient].vraag], sender);
-                }
-
-                if (sessies[recipient].vraag >= questionSet.length) {
-                    sendKlaarMessage(sender, 'alle vragen zijn beantwoord, bent u zeker over uw antwoorden?')
-                }
-
-            }
+            // if (sessies[recipient].vragensessie && questionSet) {
+            //
+            //     if (text > 10) {
+            //         sendTextMessage(sender, 'error, antwoord onbekend!')
+            //     }
+            //     if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
+            //         sessies[recipient].vraag++;
+            //         sessies[recipient].answers.push(text);
+            //         console.log(' answers zijn',sessies[recipient].answers);
+            //         // moet gereset worden + verzonden.
+            //     }
+            //
+            //     if (questionSet[sessies[recipient].vraag]) {
+            //         askQuestion(questionSet[sessies[recipient].vraag], sender);
+            //     }
+            //
+            //     if (sessies[recipient].vraag >= questionSet.length) {
+            //         sendKlaarMessage(sender, 'alle vragen zijn beantwoord, bent u zeker over uw antwoorden?')
+            //     }
+            //
+            // }
             if (event.message = null) {
                 sendTextMessage(sender, 'Het bericht word niet herkent, probeer het opnieuw of typ Help.')
             }
@@ -334,6 +334,27 @@ function startVragen(userInput)
             .catch(function (error) {
                 console.log(error);
             })
+    if (sessies[recipient].vragensessie && questionSet) {
+
+        if (text > 10) {
+            sendTextMessage(sender, 'error, antwoord onbekend!')
+        }
+        if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
+            sessies[recipient].vraag++;
+            sessies[recipient].answers.push(text);
+            console.log(' answers zijn',sessies[recipient].answers);
+            // moet gereset worden + verzonden.
+        }
+
+        if (questionSet[sessies[recipient].vraag]) {
+            askQuestion(questionSet[sessies[recipient].vraag], sender);
+        }
+
+        if (sessies[recipient].vraag >= questionSet.length) {
+            sendKlaarMessage(sender, 'alle vragen zijn beantwoord, bent u zeker over uw antwoorden?')
+        }
+
+    }
     }
 
 
