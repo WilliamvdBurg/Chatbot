e3a718032fb7c67d8397d39baf63c6e2fa756f51
William <s1079684@student.hsleiden.nl>
2016-09-29 14:47:33 +0200

    hhahahs

--- index.js
+++ index.js
@@ -100,23 +100,23 @@ app.post('/webhook/', function (req, res) {
                 })
             }
 
-            if (text.match(/^[a-zA-Z]{3,}-[0-9]{3,}/g)) {
-                console.log('werkt ');
-                var tokenAndId;
-                Promise.all([authenticateCode(text)])
-                    .then(function (result) {
-                        return tokenAndId = result[0];
-                    })
-                    .then(function (tokenAndId) {
-                        return getEvaluationData(tokenAndId.evaluationId, tokenAndId.accessToken)
-                    })
-                    .then(function (questionSet) {
-                        startVragen(questionSet, recipient);
-                    });
-            } else {
-                sendTextMessage(sender, 'Foute code boii', function () {
-                });
-            }
+            // if (text.match(/^[a-zA-Z]{3,}-[0-9]{3,}/g)) {
+            //     console.log('werkt ');
+            //     var tokenAndId;
+            //     Promise.all([authenticateCode(text)])
+            //         .then(function (result) {
+            //             return tokenAndId = result[0];
+            //         })
+            //         .then(function (tokenAndId) {
+            //             return getEvaluationData(tokenAndId.evaluationId, tokenAndId.accessToken)
+            //         })
+            //         .then(function (questionSet) {
+            //             startVragen(questionSet, recipient);
+            //         });
+            // } else {
+            //     sendTextMessage(sender, 'Foute code boii', function () {
+            //     });
+            // }
 
 
             if (text == 'Start') {
