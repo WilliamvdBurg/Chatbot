93a2f6f6b314a66d90e701b8ff5e02180a97f043
William <s1079684@student.hsleiden.nl>
2016-09-30 15:00:35 +0200

    hhahahs

--- index.js
+++ index.js
@@ -421,8 +421,7 @@ function getEvaluationData(id, accessToken) {
         var openQuestions = [];
         _.forEach(evaluation.blocks[0].questionSets, function (questionset) {
             questionSet = questionset.questions;
-            console.log('jeroentje', questionSet[0].id);
-
+            _qid = questionSet[0].id;
             console.log(questionSet);
         });
 
