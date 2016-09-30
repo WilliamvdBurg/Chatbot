117dcf94c6b0470aaf0c0b42fa6d0c3da64cfdef
William <s1079684@student.hsleiden.nl>
2016-09-30 12:30:34 +0200

    hhahahs

--- index.js
+++ index.js
@@ -156,7 +156,6 @@ app.post('/webhook/', function (req, res) {
                 }
                 if (text < 11 || text == "Eens" || text == "Oneens" || text == "Zeer weinig" || text == "Weinig" || text == "Neutraal" || text == "Veel" || text == "Zeer veel" || text == "slecht" || text == "Zeer slecht" || text == "Goed" || text == "Zeer Goed" || text == "Volledig mee oneens" || text == "Volledig mee eens") {
                     sessies[recipient].vraag++;
-                    block
                     sessies[recipient].answers.push({
                         questionId: questionSet.questions.id,
                         questionSet: 1,
