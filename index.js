bafc9a616e69a976aaeebed8ce83829e01aa1815
William <s1079684@student.hsleiden.nl>
2016-09-30 11:30:27 +0200

    hhahahs

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.post('/webhook/', function (req, res) {
                     sessies[recipient].vraag++;
 
                     sessies[recipient].answers.push({
-                        questionId: questions.id,
+                        questionId: questionSet.questions.id,
                         questionSet: data.evaluation,
                         answer: text
                     });
