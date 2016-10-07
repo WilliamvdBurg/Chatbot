2a591b5429c1599e0e013bc7d79758066d3b75a8
William <s1079684@student.hsleiden.nl>
2016-10-07 12:44:13 +0200

    hhahahs

--- index.js
+++ index.js
@@ -700,7 +700,7 @@ function sendDetails(recipient, evaluation){
     }
 
     console.log('awnsers', awnsers);
-    console.log('awnsersteachers', awnsers['awnsers'].teacher);
+    console.log('awnsersteachers', awnsers.answers['awnsers'].teacher);
     // questions.answers.push(
     //     [
     //     {
