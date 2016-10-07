89bc906b66d8a459959f0dca3defbdacb5925461
William <s1079684@student.hsleiden.nl>
2016-10-07 11:02:46 +0200

    hhahahs

--- index.js
+++ index.js
@@ -666,7 +666,7 @@ function sendDetails(recipient){
     var awnsers = [];
     awnsers['id'] = 'id';
     awnsers['topic'] = {
-        "id": evaluation.blocks[0].topic.id,
+        "id": evaluation.topic.id,
         "name": 1,
         "type": 1
     };
